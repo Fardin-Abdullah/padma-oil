0699457bf5b43f9d419a6591a696614f9047b849
munim1111 <tintaro313@gmail.com>
2024-12-13 21:39:15 +0600

    fxml repair

--- src/main/java/padmaOilCompany/qualityControlManager/QualityControlManagerDashBoardController.java
+++ src/main/java/padmaOilCompany/qualityControlManager/QualityControlManagerDashBoardController.java
@@ -1,7 +1,46 @@
 package padmaOilCompany.qualityControlManager;
 
+import javafx.event.ActionEvent;
+
 public class QualityControlManagerDashBoardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void logOutQualityControlManagerButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void conductSupplierAuditsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void performRandomBatchInspectionButtonOnActio(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ensureProductQualityComplianceButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reportNonComplaintProductsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void trackQualityMetricsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void calibrateQualityTestingButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reviewProductSpecificationsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void investigateProductComplaintsButtonOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/padmaOilCompany/supplyChainManager/SupplyChainManagerDashboardController.java
+++ src/main/java/padmaOilCompany/supplyChainManager/SupplyChainManagerDashboardController.java
@@ -7,9 +7,6 @@ public class SupplyChainManagerDashboardController
 {
 
     @javafx.fxml.FXML
-    private TextField studentIdTextField;
-
-    @javafx.fxml.FXML
     public void initialize() {
     }
 
@@ -17,7 +14,43 @@ public class SupplyChainManagerDashboardController
     public void INiButtonOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void searchButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void scheduleDeliveriesButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void supplyChainReportsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDeliveryIssesButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void optimizeSupplyChainRoutesButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageInventoryLevelsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void monitorOIlShipmentButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reviewSupplierContractsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void coordinateWithLevelsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LogOutSupplyChainManagerButtonOnAction(ActionEvent actionEvent) {
     }
 }
